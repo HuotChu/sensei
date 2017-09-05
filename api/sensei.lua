91bdbee48ec70fd9a0a49a3dfcd47b11481bce98
blujagu <lawebtek@gmail.com>
2017-09-04 22:50:48 -0400

    api/sensei stub

---
+++ api/sensei.lua
@@ -0,0 +1,13 @@
+--- module:
+  --	api/sensei
+  -- summary:
+  --	This is the main module for the sensei framework; it loads sensei base and prepares the execution environment
+
+--[[ Comment functions using the LuaDoc format below... ]]--
+
+--- Summary ends with a period.
+-- Some description, can be over several lines.
+-- @param p1 first parameter
+-- @param p2 second parameter
+-- @return a string value
+-- @see second_fun
